51fc8d2f0c9de537e9c15bba405d02ba35255016
anromero21 <60559405+anromero21@users.noreply.github.com>
2020-07-01 18:05:07 -0500

    First commit
    Arduino Project "Códigos de error"

---
+++ Códigos de error/Códigos de error/Códigos de error.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Códigos_de_error.ino
+ Created:	01/07/2020 17:58:05
+ Author:	angel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
