20ae465708d88645a10a4b6b783808f2dbfdbffe
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-07-27 17:25:17 -0500

    First commit

--- SIM800L_IOT/SIM800L_IOT/SIM800L_IOT.ino
+++ SIM800L_IOT/SIM800L_IOT/SIM800L_IOT.ino
@@ -1,9 +1,8 @@
 /*
  Name:		SIM800L_IOT.ino
- Created:	27/07/2020 16:04:20
+ Created:	27/07/2020 17:24:11
  Author:	YARIB
 */
-
 #include <SoftwareSerial.h>
 SoftwareSerial gprsSerial(3, 2);
 
@@ -13,106 +12,12 @@ SoftwareSerial gprsSerial(3, 2);
 #define DHTPIN 4
 
 DHT dht(DHTPIN, DHT11);
+// the setup function runs once when you press reset or power the board
+void setup() {
 
-void setup()
-{
-	gprsSerial.begin(9600);               // the GPRS baud rate   
-	Serial.begin(9600);    // the GPRS baud rate 
-	dht.begin();
-
-	delay(1000);
 }
 
-void loop()
-{
-	float h = dht.readHumidity();
-	float t = dht.readTemperature();
-	delay(100);
-
-	Serial.print("Temperature = ");
-	Serial.print(t);
-	Serial.println(" °C");
-	Serial.print("Humidity = ");
-	Serial.print(h);
-	Serial.println(" %");
-
-
-	if (gprsSerial.available())
-		Serial.write(gprsSerial.read());
-
-	gprsSerial.println("AT");
-	delay(1000);
-
-	gprsSerial.println("AT+CPIN?");
-	delay(1000);
-
-	gprsSerial.println("AT+CREG?");
-	delay(1000);
-
-	gprsSerial.println("AT+CGATT?");
-	delay(1000);
-
-	gprsSerial.println("AT+CIPSHUT");
-	delay(1000);
-
-	gprsSerial.println("AT+CIPSTATUS");
-	delay(2000);
-
-	gprsSerial.println("AT+CIPMUX=0");
-	delay(2000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CSTT=\"internet.itelcel.com\"");//start task and setting the APN,
-	delay(1000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIICR");//bring up wireless connection
-	delay(3000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIFSR");//get local IP adress
-	delay(2000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIPSPRT=0");
-	delay(3000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
-	delay(6000);
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIPSEND");//begin send data to remote server
-	delay(4000);
-	ShowSerialData();
-
-	String str = "GET https://api.thingspeak.com/update?api_key=ULT9XDI0JGNT8X77&field1=" + String(t) + "&field2=" + String(h);
-	Serial.println(str);
-	gprsSerial.println(str);//begin send data to remote server
-
-	delay(4000);
-	ShowSerialData();
-
-	gprsSerial.println((char)26);//sending
-	delay(5000);//waitting for reply, important! the time is base on the condition of internet 
-	gprsSerial.println();
-
-	ShowSerialData();
-
-	gprsSerial.println("AT+CIPSHUT");//close the connection
-	delay(100);
-	ShowSerialData();
-}
-void ShowSerialData()
-{
-	while (gprsSerial.available() != 0)
-		Serial.write(gprsSerial.read());
-	delay(5000);
-
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
 }
