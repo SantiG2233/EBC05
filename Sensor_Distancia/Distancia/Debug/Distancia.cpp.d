026c43d6dacdf6deab7c61eca9310b34698b3a32
Sara <sara_camargom@hotmail.com>
2020-07-29 11:51:31 -0500

    inclui la libreria

---
+++ Sensor_Distancia/Distancia/Debug/Distancia.cpp.d
@@ -0,0 +1,26 @@
+C:\Users\Dayan\AppData\Local\Temp\VMBuilds\Distancia\uno\Debug\Distancia.cpp.o: \
+ C:\Users\Dayan\AppData\Local\Temp\VMBuilds\Distancia\uno\Debug\Distancia.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\opspbqse.n3l\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/Adafruit_VL53L0X.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_api.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_api_strings.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_def.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_device.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_types.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_platform.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_i2c_platform.h \
+ C:\Users\Dayan\Documents\Arduino\libraries\Adafruit_VL53L0X\src/vl53l0x_platform_log.h
---
+++ Sensor_Distancia/Distancia/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\Dayan\AppData\Local\Temp\VMBuilds\Distancia\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\opspbqse.n3l\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
